577276fa5026541850d92c61fc38a11be71071a8
Estel Zoe <estelzoe433@gmail.com>
2025-06-03 10:08:17 +0000

    first commit of event-hive

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer"
+import NavBar from "./components/navbar"
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="text-center 4xl font-bold">Welcome to Event Hive</h1>
+     <Footer>
+
+     </Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import { Linkedin, Instagram, Facebook } from "lucide-react"
+
+
+function Footer() {
+    return (
+        <footer className="bg-navy-blue text-white">
+            <h1 className="flex justify-center 4xl text-border gap-2"><span>Event</span><span className="text-blue-800">Hive</span></h1>
+            <form>
+                <input type="email" placeholder="Enter your mail" name="email" />
+                <button type="submit">Subscribe</button>
+            </form>
+            <div>
+                <span>Home</span>
+                <span>About</span>
+                <span>Services</span>
+                <span>Get In Touch</span>
+                <span>FAQs</span>
+            </div>
+            <div>
+                <div>
+                    <button>English</button>
+                    <button>French</button>
+                    <button>Hindi</button>
+                </div>
+                <Linkedin/>
+                <Instagram/>
+                <Facebook/>
+
+            </div>
+            <p>Non Copyrighted© 2025 Upload By EventHive</p>
+        </footer>
+    );
+}
+export default Footer;
