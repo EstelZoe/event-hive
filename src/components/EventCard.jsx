14d8787554ab4be15d8b29eb0ba13b3b5821e609
Estel Zoe <estelzoe433@gmail.com>
2025-06-04 15:42:25 +0000

    did my state react

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,10 +2,10 @@ import eventCardImage from "../assets/images/eventcard.png";
 
 
 
-export default function EventCard() {
+export default function EventCard({event}) {
     return(
         <div>
-          <img src={eventCardImage} alt="Event Card" />
+          <img src={event.image} alt="Event Card" />
             <h1>BestSeller Book Bootcamp -write, & Publish Your Book-Lucknow</h1>
             <h2>Saturday, March 18, 9.30PM</h2>
             <h3>ONLINE EVENT-Attend anywhere</h3>
